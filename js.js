79e67d7373b9324d81d553c5e0bb5abf5236c5be
Paul Watkins <paulhwatkins@gmail.com>
2017-04-26 20:41:03 +0000

    Starting from scratch

--- js.js
+++ js.js
@@ -2,47 +2,5 @@
 var dimensions = 16;
 
 $(document).ready(function(){
-   function createBlocks(){
-       for (var i = 0; i < dimensions; i++)
-       {
-           for (var j=0; j < dimensions; j++)
-           {
-               $('grid').append("<div class='unit'></div>");
-           }
-       }
-   }
-   createBlocks();
-   
-   $('#reset').click(function(){
-       $('.unit').css('background-color','yellow');
-       
-       dimensions = prompt("Enter a number 1-64","");
-       
-       if (isNAN(dimensions))
-            alert("Not a number");
-        else if (dimensions < 1)
-            dimensions = 1;
-        else (dimensions > 64)
-            dimensions = 64;
-            
-    var size = 512 / dimensions;
-    
-    function reset() {
-        $('.unit').remove();
-        createBlocks();
-    }
-        reset();
-        
-        $('.unit').css({'width':size+'px', 'height':size+'px'});
-        
-        $('unit').hover(function() {
-            $(this).css('background-color', '#000');
-    });
+  
 });
-
-    $('.unit').hover(function() {
-        $(this).css('background-color', '#000');
-    });
-    
-});
-
