cf2d3e28f789a37b700661585e52949538b4c1d3
Paul Watkins <paulhwatkins@gmail.com>
2017-04-26 18:54:53 +0000

    Odin Answer Ana

--- js.js
+++ js.js
@@ -1,15 +1,48 @@
 /*Javascript Practice*/
-var rows = 8;
-var columns = 8;
-var $row = $("<div>", {class: 'row'});
-var $unit = $("<div>", {class: 'unit'});
+var dimensions = 16;
 
 $(document).ready(function(){
-    for(var i = 0; i < rows; i++){      
-            $("#container").append($row);
+   function createBlocks(){
+       for (var i = 0; i < dimensions; i++)
+       {
+           for (var j=0; j < dimensions; j++)
+           {
+               $('grid').append("<div class='unit'></div>");
+           }
+       }
+   }
+   createBlocks();
+   
+   $('#reset').click(function(){
+       $('.unit').css('background-color','yellow');
+       
+       dimensions = prompt("Enter a number 1-64","");
+       
+       if (isNAN(dimensions))
+            alert("Not a number");
+        else if (dimensions < 1)
+            dimensions = 1;
+        else (dimensions > 64)
+            dimensions = 64;
+            
+    var size = 512 / dimensions;
+    
+    function reset() {
+        $('.unit').remove();
+        createBlocks();
     }
-
-    for(var i = 0; i < columns; i++){
-            $(".row").append($unit);
-    }
+        reset();
+        
+        $('.unit').css({'width':size+'px', 'height':size+'px'});
+        
+        $('unit').hover(function() {
+            $(this).css('background-color', '#000');
+    });
 });
+
+    $('.unit').hover(function() {
+        $(this).css('background-color', '#000');
+    });
+    
+});
+
